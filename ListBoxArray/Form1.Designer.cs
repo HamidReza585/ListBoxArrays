94e293bca5697ae7ecff163d2cf232a616002d84
HamidReza <57710259+HamidReza585@users.noreply.github.com>
2021-02-19 19:23:52 +0330

    Added Files

---
+++ ListBoxArray/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace ListBoxArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txtRemove = new System.Windows.Forms.TextBox();
+            this.btnRemoveAt = new System.Windows.Forms.Button();
+            this.txtRemoveAt = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNames
+            // 
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.Location = new System.Drawing.Point(12, 12);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(229, 251);
+            this.lstNames.TabIndex = 0;
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(247, 12);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(223, 20);
+            this.txtAdd.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(247, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(223, 30);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(247, 102);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(223, 30);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txtRemove
+            // 
+            this.txtRemove.Location = new System.Drawing.Point(247, 75);
+            this.txtRemove.Name = "txtRemove";
+            this.txtRemove.Size = new System.Drawing.Size(223, 20);
+            this.txtRemove.TabIndex = 3;
+            // 
+            // btnRemoveAt
+            // 
+            this.btnRemoveAt.Location = new System.Drawing.Point(247, 168);
+            this.btnRemoveAt.Name = "btnRemoveAt";
+            this.btnRemoveAt.Size = new System.Drawing.Size(223, 30);
+            this.btnRemoveAt.TabIndex = 6;
+            this.btnRemoveAt.Text = "Remove At";
+            this.btnRemoveAt.UseVisualStyleBackColor = true;
+            this.btnRemoveAt.Click += new System.EventHandler(this.btnRemoveAt_Click);
+            // 
+            // txtRemoveAt
+            // 
+            this.txtRemoveAt.Location = new System.Drawing.Point(247, 141);
+            this.txtRemoveAt.Name = "txtRemoveAt";
+            this.txtRemoveAt.Size = new System.Drawing.Size(223, 20);
+            this.txtRemoveAt.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(247, 234);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(223, 30);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(247, 207);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(223, 20);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(12, 269);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(458, 28);
+            this.btnRandom.TabIndex = 9;
+            this.btnRandom.Text = "Add Random Names";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 303);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(458, 28);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 339);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnRemoveAt);
+            this.Controls.Add(this.txtRemoveAt);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.lstNames);
+            this.Name = "Form1";
+            this.Text = "ListBoxes And Arrays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNames;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtRemove;
+        private System.Windows.Forms.Button btnRemoveAt;
+        private System.Windows.Forms.TextBox txtRemoveAt;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ListBoxArray/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtAdd.Text != "")
+                lstNames.Items.Add(txtAdd.Text);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (txtRemove.Text != "" && lstNames.Items.Contains(txtRemove.Text))
+                lstNames.Items.Remove(txtRemove.Text);
+        }
+
+        private void btnRemoveAt_Click(object sender, EventArgs e)
+        {
+            lstNames.Items.RemoveAt(int.Parse(txtRemoveAt.Text));
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (lstNames.Items.Contains(txtSearch.Text))
+                MessageBox.Show("Found");
+            else
+                MessageBox.Show("Not Found");
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            string[] names = new string[10] {"Jack", "Rick", "Tom", "Tim", "George", "Rudy", "Selena", "Ariana", "Athena", "James"};
+            Random rnd = new Random();
+            string randomName = names[rnd.Next(10)];
+            lstNames.Items.Add(randomName);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstNames.Items.Clear();
+        }
+    }
+}
